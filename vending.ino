f9d1e79134e850af4dcf0b6b06ac1f0a5b3c35bf
Albert Ye <albert.ye.728@gmail.com>
2022-02-16 12:17:17 -0800

    added motor pseudocode

--- vending.ino
+++ vending.ino
@@ -1,13 +1,84 @@
 #include <LiquidCrystal.h>
+#include <Stepper.h>
+#include <Servo.h>
 
 LiquidCrystal lcd(12,11,5,4,3,2);
 int but = 6, pir = 10;
 int but2 = 7, but3 = 8, but4 = 9; // and all of them must link to a stepper motor, which we'll get later
 bool wait = 0;
 
+int mp1 = 13, mp2 = 14, mp3 = 15, mp4 = 16, steps = 2048;
+Stepper stepper = Stepper(steps, mp1, mp2, mp3, mp4);
+Servo s[4];
+
+int x[4] = {69, 69, 69, 69};
+int y[4] = {69, 69, 69, 69};
+int xn = 69, yn = 69; // neutral position, at the bottom near the recepticle
+
+void move_to(int i)
+{
+  int sx = x[i] - xn;
+  int sy = y[i] - xn;
+  // number of steps between neutral and the ith slot
+  // set direction to 0 if sx < 0 amd 1 if sx > 0
+  // move horizontal stepper sx steps
+  for(int i = 0; i < sx; i++)
+  {
+    // start move here
+    delayMicroseconds(300);
+    // stop move here
+    delayMicroseconds(300);
+  }
+  // repeat the same with y
+  for(int i = 0; i < sy; i++)
+  {
+    // start
+    delayMicroseconds(300);
+    // stop
+    delayMicroseconds(300);
+  }
+}
+
+void move_from(int i)
+{
+  int sx = xn - x[i];
+  int sy = yn - y[i];
+  // number of steps between neutral and the ith slot
+  // set direction to 0 if sx < 0 amd 1 if sx > 0
+  // move horizontal stepper sx steps
+  for(int i = 0; i < sx; i++)
+  {
+    // start move here
+    delayMicroseconds(300);
+    // stop move here
+    delayMicroseconds(300);
+  }
+  // repeat the same with y
+  for(int i = 0; i < sy; i++)
+  {
+    // start
+    delayMicroseconds(300);
+    // stop
+    delayMicroseconds(300);
+  }
+}
+
+void move_motor(int i)
+{
+  // move motor to index x[i], y[i] specifically
+  move_to(i);
+  // rotate as needed with servo
+  s[i].writeMicroseconds(2000);
+  delay(950);
+  s[i].writeMicroseconds(1500);
+  delay(500);
+  // move motor back
+}
+
 void setup() {
   // put your setup code here, to run once:
   wait = 0;
+  // initialize the LCD and the PIR
   lcd.begin(16,2);
   lcd.clear();
   lcd.print("Initializing PIR");
@@ -32,6 +103,9 @@ void setup() {
   lcd.print("dollars now");
   pinMode(but, INPUT);
   pinMode(pir, INPUT);
+
+  // initialize stepper motor
+  stepper.setSpeed(8);
 }
 
 int pre = 0, pre2 = 0, pre3 = 0, pre4 = 0;
