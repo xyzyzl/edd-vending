928655aee0dfb5a81991f6e4f4400f6ec0a0178c
jhu04 <54165683+jhu04@users.noreply.github.com>
2022-03-09 12:24:36 -0800

    Bugged 4 stepper code
    Bugged 4 stepper code, worked for 2 steppers a bit earlier

--- steptest/steptest.ino
+++ steptest/steptest.ino
@@ -1,16 +1,21 @@
 #include <CheapStepper.h>
 
-CheapStepper stepper[2] = {CheapStepper(13, 12, 11, 10),
-                          CheapStepper(9, 8, 7, 6)};
-//CheapStepper debugStepper(10, 11, 12, 13);
+CheapStepper stepper[4] = {CheapStepper(13, 12, 11, 10),
+                          CheapStepper(9, 8, 7, 6),
+                          CheapStepper(5, 4, 3, 2),
+                          CheapStepper(14, 15, 16, 17)};
 
 const int buttonPins[4] = {50, 51, 52, 53};
 
-boolean moveClockwise = true;
+boolean cw = true;
+boolean ccw = false;
 
 void setup() {
-    for (int i = 0; i < 2; ++i)
+    for (int i = 0; i < 4; ++i)
+    {
         stepper[i].setRpm(20);
+        stepper[i].moveTo(ccw, 0);
+    }
         
     Serial.begin(9600);
 //    Serial.print("rpm = delay of ");
@@ -25,11 +30,14 @@ void setup() {
 void loop() {
     int buttonStates[4];
 
-    for (int i = 0; i < 2; ++i)
+    for (int i = 0; i < 4; ++i)
     {
         buttonStates[i] = digitalRead(buttonPins[i]);
-//        if (buttonStates[i] && i < 2)
-            stepper[i].moveDegrees(moveClockwise, 90);
+
+//        Serial.println(i + ": " + buttonStates[i]);
+        
+        if (buttonStates[i])
+            stepper[i].moveDegrees(ccw, 90);
     }
 
 //    debugStepper.moveDegrees(true, 90);
