77cd4247e7e13c4149d20ba3cd7ef2c6b0742204
jhu04 <54165683+jhu04@users.noreply.github.com>
2022-03-19 21:06:30 -0700

    Working Spaghetti Code
    Working spaghetti code with debug statements. The problem was that a motor was broken (hardware, not software problem), so the previous commit likely works as well

--- steptest/steptest.ino
+++ steptest/steptest.ino
@@ -1,9 +1,55 @@
 #include <CheapStepper.h>
 
-CheapStepper stepper[4] = {CheapStepper(13, 12, 11, 10),
+
+//CheapStepper stepper[24] = {CheapStepper(10,11,12,13),
+//CheapStepper(11,10,12,13),
+//CheapStepper(12,10,11,13),
+//CheapStepper(10,12,11,13),
+//CheapStepper(11,12,10,13),
+//CheapStepper(12,11,10,13),
+//CheapStepper(12,11,13,10),
+//CheapStepper(11,12,13,10),
+//CheapStepper(13,12,11,10),
+//CheapStepper(12,13,11,10),
+//CheapStepper(11,13,12,10),
+//CheapStepper(13,11,12,10),
+//CheapStepper(13,10,12,11),
+//CheapStepper(10,13,12,11),
+//CheapStepper(12,13,10,11),
+//CheapStepper(13,12,10,11),
+//CheapStepper(10,12,13,11),
+//CheapStepper(12,10,13,11),
+//CheapStepper(11,10,13,12),
+//CheapStepper(10,11,13,12),
+//CheapStepper(13,11,10,12),
+//CheapStepper(11,13,10,12),
+//CheapStepper(10,13,11,12),
+//CheapStepper(13,10,11,12)
+//}; //5 7 8 13 14
+
+// this does not work ???
+//CheapStepper stepper[24] = {CheapStepper(10,11,12,13),
+//CheapStepper(11,10,12,13),
+//CheapStepper(12,10,11,13),
+//CheapStepper(10,12,11,13),
+//CheapStepper(11,12,10,13),
+//CheapStepper(12,11,10,13)};
+
+//CheapStepper stepper(12,11,10,13);
+
+CheapStepper stepper[4] = {
+    CheapStepper(12,11,10,13),
+    CheapStepper(8, 7, 6, 9),
+    CheapStepper(4, 3, 2, 5),
+    CheapStepper(24, 26, 28, 22)
+};
+
+/*
                           CheapStepper(9, 8, 7, 6),
                           CheapStepper(5, 4, 3, 2),
-                          CheapStepper(14, 15, 16, 17)};
+                          CheapStepper(14, 15, 16, 17)};*/
+
+const int stepperLength = sizeof stepper / sizeof stepper[0];
 
 const int buttonPins[4] = {50, 51, 52, 53};
 
@@ -30,15 +76,18 @@ void setup() {
 void loop() {
     int buttonStates[4];
 
-    for (int i = 0; i < 4; ++i)
+    for (int i = 0; i < stepperLength; ++i)
     {
-        buttonStates[i] = digitalRead(buttonPins[i]);
+//        buttonStates[i] = digitalRead(buttonPins[i]);
 
-//        Serial.println(i + ": " + buttonStates[i]);
+//        Serial.print((String)i + ": " + (String)buttonStates[i] + "    ");
         
-        if (buttonStates[i])
+//        if (buttonStates[i])
+//            Serial.println(i);
             stepper[i].moveDegrees(ccw, 90);
     }
+
+    Serial.println();
 
 //    debugStepper.moveDegrees(true, 90);
 }
