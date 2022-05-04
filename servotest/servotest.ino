76d17e08454145e690a485c622462f07218fea19
Jeffrey Hu <jeffreyhu8@gmail.com>
2022-05-04 11:52:35 -0700

    servotest

--- servotest/servotest.ino
+++ servotest/servotest.ino
@@ -1,27 +1,70 @@
 #include <Servo.h>
 
-Servo s[4];
-Servo s0, s1, s2, s3;
-int but[4] = {6, 7, 8, 9};
+// speed of servo
+const int v = 8;
 
-int v = 10;
+// pins
+const int ser[4] = {22, 24, 26, 28};
+const int but[4] = {6, 7, 8, 9};
+const int PIRPin = 5;
+
+// servos, doesn't work in array for some reason I think
+Servo s0, s1, s2, s3;
+
+// whether or not coin has been inserted
+bool paid = false;
 
 void setup() {
-  // put your setup code here, to run once:
-  s0.attach(22);
-  s1.attach(23);
-  s2.attach(24);
-  s3.attach(25);
+    // debug
+    Serial.begin(9600);
+    
+    for (int i = 0; i < 4; ++i)
+    {
+        pinMode(ser[i], OUTPUT);
+        pinMode(but[i], INPUT);
+    }
+    
+    s0.attach(22);
+    s1.attach(24);
+    s2.attach(26);
+    s3.attach(28);
+    
+    s0.write(90);
+    s1.write(90);
+    s2.write(90);
+    s3.write(90);
 
-  s0.write(90);
-  s1.write(90);
-  s2.write(90);
-  s3.write(90);
-  
-  Serial.begin(9600);
+    // PIR initialization
+    Serial.print("Intializing sensor ");
+    for(int x = 0; x < 20; x++) //Initialization lasts for 20 sec
+    {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println(" done");
+    Serial.println("SENSOR ACTIVE");
 }
 
 void loop() {
+    if (!paid)
+    {
+        if (digitalRead(PIRPin))
+        {
+            paid = true;
+            Serial.println("Coin detected!");
+        }
+        return;
+    }
+    
+    bool buttonPressed = false;
+    for (int i = 0; i < 4; ++i)
+        buttonPressed = buttonPressed || digitalRead(but[i]);
+    if (buttonPressed)
+    {
+        Serial.println("Button pressed! Dispensing item...");
+        paid = false;
+    }
+
     if (digitalRead(but[0]))
         s0.write(90 + v);
     else
@@ -42,12 +85,10 @@ void loop() {
     else
         s3.write(90);
 
-    bool buttonPressed = false;
-    for (int i = 0; i < 4; ++i)
-        buttonPressed = buttonPressed || digitalRead(but[i]);
     if (buttonPressed)
         delay(1000);
 
-  Serial.println("Update:");
-  Serial.println(s0.read());
+    // debug
+//  Serial.println("Update:");
+//  Serial.println(s0.read());
 }
